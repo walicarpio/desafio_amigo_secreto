f6884d0d274d4b1cd367de9b6669035ba8eebfa0
Walicarpio <eberrios@gmail.com>
2025-03-07 21:39:24 -0300

    Creando un array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
 // Aquí deberás desarrollar la lógica para resolver el problema.
+
+// CREANDO UN ARRAY PARA ALMACENAR LOS NOMBRES
+let amigos = [];
