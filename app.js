179859b2acbec10ad718e00fc6f9baedbe054e6f
Walicarpio <eberrios@gmail.com>
2025-03-07 21:42:02 -0300

    Función para agregar amigos

--- app.js
+++ app.js
@@ -12,3 +12,29 @@ let li;
 let indiceAleatorio;
 let amigoSecreto;
 let resultado;
+
+// FUNCIÓN PARA AGREGAR AMIGOS
+function agregarAmigo() {
+  ulListaAmigos; // Capturando el valor del campo de entrada
+  nombre = inputAmigo.value.trim(); // Eliminando espacios en blanco al inicio y al final
+
+  // Validar la entrada. Implementando una validación para asegurarse que el campo no esté vacío
+  if (nombre === "") {
+    alert("El campo está vacío. Por favor, inserta un nombre para sortear.");
+    return;
+  }
+  // Validando si el nombre ya está en la lista
+  if (amigos.includes(nombre)) {
+    alert(`Oppssss! El nombre ${nombre} ya está en la lista`);
+    return;
+  }
+
+  // Agregar el nombre al array de amigos
+  amigos.push(nombre);
+
+  // Actualizando la lista de amigos
+  actualizarLista();
+
+  // Limpiando el campo de entrada
+  inputAmigo.value = "";
+}
